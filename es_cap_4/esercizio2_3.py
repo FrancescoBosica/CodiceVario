99d9386deaea6a45d2362ed84a7ba77c2a56c62c
FrancescoBosica <104219876+FrancescoBosica@users.noreply.github.com>
2023-06-28 22:11:14 +0200

    Create esercizio2_3.py

---
+++ es_cap_4/esercizio2_3.py
@@ -0,0 +1,11 @@
+#esercizio 2
+#repeat_lyrics()
+#esercizio 3
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+#main
+repeat_lyrics()
