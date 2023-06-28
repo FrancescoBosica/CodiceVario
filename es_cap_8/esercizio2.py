4dc553225d3cd44d66ca228b51db1585012bfb69
FrancescoBosica <104219876+FrancescoBosica@users.noreply.github.com>
2023-06-28 22:11:45 +0200

    Create esercizio2.py

---
+++ es_cap_8/esercizio2.py
@@ -0,0 +1,8 @@
+fhand = open('D:\studio\python\programmi\es_cap_8\mbox-short.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    # print 'Debug:', words
+    if len(words) == 0 : continue
+    if words[0] != 'From' : continue
+print(words[2])
